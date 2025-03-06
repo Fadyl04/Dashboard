87a10335340eae2510b672211e583e10b284ee65
Fadyl <fadylbouraima4@gmail.com>
2025-03-06 05:55:16 +0100

    Dashboard

--- script.js
+++ script.js
@@ -78,4 +78,19 @@ switchMode.addEventListener('change', function () {
 	} else {
 		document.body.classList.remove('dark');
 	}
-})
+});
+
+document.getElementById("togglePassword").addEventListener("click", function() {
+	let passwordField = document.getElementById("password");
+	let toggleIcon = this;
+
+	if (passwordField.type === "password") {
+		passwordField.type = "text";
+		toggleIcon.classList.remove("bx-hide");
+		toggleIcon.classList.add("bxs-show"); // Changer l'icône
+	} else {
+		passwordField.type = "password";
+		toggleIcon.classList.remove("bxs-show");
+		toggleIcon.classList.add("bx-hide"); // Revenir à l'icône cachée
+	}
+});
